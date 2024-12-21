627027f9a516201860504a3d8acfa71a26d45355
Ratnesh kumar tiwari <retneshtiwari@gmail.com>
2024-12-21 23:48:30 +0530

    Cleaned up our About us

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,39 @@
+import apj from "../assets/Images/apj.webp";
+import billGates from "../assets/Images/billGates.jpg";
+import einstein from "../assets/Images/einstein.jpg";
+import nelsonMandela from "../assets/Images/nelsonMandela.jpg";
+import steveJobs from "../assets/Images/steveJobs.jpg";
+
+export const celebrities = [
+    {
+        title:" Nelson Mandela",
+        description:"Education is the most powerful tool you can use to change the world.",
+        image:nelsonMandela,
+        slideNumber: 1
+    },
+    {
+        title:" APJ Abdul Kalam",
+        description:"Dream is not that which you see while sleeping it is something that does not let you sleep.",
+        image:apj,
+        slideNumber: 2
+    },
+    {
+        title:"Albert Einstein",
+        description:"Life is like riding a bicycle. To keep your balance, you must keep moving.",
+        image:einstein,
+        slideNumber: 3
+    },
+    {
+        title:"Steve Jobs",
+        description:"Great things in business are never done by one person. They're done by a team of people.",
+        image:steveJobs,
+        slideNumber: 4
+    },
+    {
+        title:"Bill Gates",
+        description:"Don't compare yourself with anyone in this world ... if you do so, you are insulting yourself.",
+        image:billGates,
+        slideNumber: 5
+    },
+
+]
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,13 +1,11 @@
 import HomeLayout from "../Layouts/HomeLayout";
 import aboutMainImage from "../assets/Images/aboutMainImage.webp"
-import apj from "../assets/Images/apj.webp";
-import billGates from "../assets/Images/billGates.jpg";
-import einstein from "../assets/Images/einstein.jpg";
-import nelsonMandela from "../assets/Images/nelsonMandela.jpg";
-import steveJobs from "../assets/Images/steveJobs.jpg";
+import { celebrities } from "../Constants/CelebrityData";
+import CarouselSlide from "../Components/carouselSlide";
 
 
 function AboutUs(){
+   
     return(
         <HomeLayout>
             <div className="pl-20 pt-20 flex flex-col text-white">
@@ -35,88 +33,18 @@ function AboutUs(){
                         />
                     </div>
                 </div>
-            </div>
+            
 
-            <div className="carousel w-1/2 my-16 m-auto">
-            <div id="slide1" className="carousel-item relative w-full">
-                <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                <img
-                src={nelsonMandela}
-                className="w-40 rounded-full border-2 border-gray-400" />
-                <p className="text-xl text-gray-200">
-                    {"Education is the most powerful tool you can use to change the world."}
-                </p>
-                <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide5" className="btn btn-circle">❮</a>
-                <a href="#slide2" className="btn btn-circle">❯</a>
-              </div>
-                </div>
-              
+              <div className="carousel w-1/2 my-16 m-auto">
+                {celebrities && celebrities.map(celebrity =>(<CarouselSlide
+                                                             {...celebrity}
+                                                             key={celebrity.slideNumber}
+                                                             totalSlides={celebrities.length}
+                                                              />))}
+               
+           
+               </div>
             </div>
-           <div id="slide2" className="carousel-item relative w-full">
-           <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                <img
-                src={apj}
-                className="w-40 rounded-full border-2 border-gray-400" />
-                 <p className="text-xl text-gray-200">
-                    {"Dream is not that which you see while sleeping it is something that does not let you sleep."}
-                </p>
-                <h3 className="text-2xl font-semibold">APJ Abdul Kalam</h3>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide1" className="btn btn-circle">❮</a>
-                <a href="#slide3" className="btn btn-circle">❯</a>
-              </div>
-                </div>
-            </div>
-
-            <div id="slide3" className="carousel-item relative w-full">
-              <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                <img
-                src={einstein}
-                className="w-40 rounded-full border-2 border-gray-400" />
-                 <p className="text-xl text-gray-200">
-                    {"Life is like riding a bicycle. To keep your balance, you must keep moving."}
-                </p>
-                <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-                   <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                       <a href="#slide2" className="btn btn-circle">❮</a>
-                       <a href="#slide4" className="btn btn-circle">❯</a>
-                   </div>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                <img
-                src={steveJobs}
-                className="w-40 rounded-full border-2 border-gray-400" />
-                 <p className="text-xl text-gray-200">
-                    {"Great things in business are never done by one person. They're done by a team of people."}
-                </p>
-                <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                       <a href="#slide3" className="btn btn-circle">❮</a>
-                       <a href="#slide5" className="btn btn-circle">❯</a>
-                   </div>
-                </div>
-            </div>
-            <div id="slide5" className="carousel-item relative w-full">
-                <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                   <img
-                      src={billGates}
-                      className="w-40 rounded-full border-2 border-gray-400" 
-                    />
-                     <p className="text-xl text-gray-200">
-                    {"Don't compare yourself with anyone in this world ... if you do so, you are insulting yourself."}
-                </p>
-                <h3 className="text-2xl font-semibold">Bill Gates</h3>
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                         <a href="#slide4" className="btn btn-circle">❮</a>
-                         <a href="#slide1" className="btn btn-circle">❯</a>
-                   </div>
-                </div>
-            </div>
-          </div>
         </HomeLayout>
     );
 }

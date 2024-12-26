6ab2068202be996476a45dc7af64b470b4d96051
Ratnesh kumar tiwari <retneshtiwari@gmail.com>
2024-12-27 00:08:09 +0530

    Added denied page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import CourseList from './Pages/Course/courseList';
 import Contact from './Pages/Contact';
+import Denied from './Pages/Denied';
 function App() {
   return (
     <> 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/login" element = {<Login />}></Route>
             <Route path="/courses" element = {<CourseList />}></Route>
             <Route path="/contact" element = {<Contact />}></Route>
+            <Route path="/denied" element = {<Denied />}></Route>
 
             <Route path="*" element = {<NotFound />}></Route>
           </Routes>

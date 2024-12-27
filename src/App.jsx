70029a1460d5aab19eb996712d5922af405d74f0
Ratnesh kumar tiwari <retneshtiwari@gmail.com>
2024-12-27 21:28:50 +0530

    Prepared course details component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './Pages/Login';
 import CourseList from './Pages/Course/courseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/Course/CourseDescription';
 function App() {
   return (
     <> 
@@ -20,6 +21,7 @@ function App() {
             <Route path="/courses" element = {<CourseList />}></Route>
             <Route path="/contact" element = {<Contact />}></Route>
             <Route path="/denied" element = {<Denied />}></Route>
+            <Route path="/course/description" element = {<CourseDescription />}></Route>
 
             <Route path="*" element = {<NotFound />}></Route>
           </Routes>
